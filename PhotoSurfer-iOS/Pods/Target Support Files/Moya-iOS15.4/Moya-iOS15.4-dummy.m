801c4d8b5b4cfcb0100230de45278da832ea86b4
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-07-12 00:18:30 +0900

    [#11] Moya, Kingfisher, IQKeyboardManager, Lottie 추가

---
+++ PhotoSurfer-iOS/Pods/Target Support Files/Moya-iOS15.4/Moya-iOS15.4-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Moya_iOS15_4 : NSObject
+@end
+@implementation PodsDummy_Moya_iOS15_4
+@end
---
+++ PhotoSurfer-iOS/Pods/Target Support Files/Pods-ImageShareExtension/Pods-ImageShareExtension-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ImageShareExtension : NSObject
+@end
+@implementation PodsDummy_Pods_ImageShareExtension
+@end
