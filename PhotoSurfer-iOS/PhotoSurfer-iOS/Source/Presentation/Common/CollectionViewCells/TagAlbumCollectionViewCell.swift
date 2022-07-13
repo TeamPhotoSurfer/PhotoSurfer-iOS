0fe8ea488e0062275a3ff82e0e45fa29c7f1eebf
jeongkite <jeong6kite@gmail.com>
2022-07-14 06:04:59 +0900

    [#10] 태그 앨범 컬렉션뷰

--- PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Common/CollectionViewCells/TagAlbumCollectionViewCell.swift
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Common/CollectionViewCells/TagAlbumCollectionViewCell.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class TagAlbumCollectionViewCell: UICollectionViewCell {
-
+    
+    // MARK: - IBOutlet
+    @IBOutlet weak var tagBackgroundImage: UIImageView!
+    @IBOutlet weak var tagStarImage: UIImageView!
+    @IBOutlet weak var tagNameLabel: UILabel!
+    
+    // MARK: - LifeCycle
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
-
+    
+    // MARK: - Function
+    func setDumy(_ album: Album) {
+        if album.isMarked {
+            tagStarImage.image = Const.Image.leftStarIconYellowButton
+        }
+        tagNameLabel.text = album.name
+    }
 }
--- PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Tag/TagViewController.swift
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Tag/TagViewController.swift
@@ -7,12 +7,22 @@
 
 import UIKit
 
+struct Album: Hashable {
+    let isMarked: Bool
+    let name: String
+}
+
 class TagViewController: UIViewController {
+    enum Section {
+        case main
+    }
 
     // MARK: - Property
-    
+    var dataSource: UICollectionViewDiffableDataSource<Section, Album>!
+    var albumList: [Album] = Album.list
     
     // MARK: - IBOutlet
+    @IBOutlet weak var albumCollectionView: UICollectionView!
     
     
     // MARK: - LifeCycle
@@ -23,12 +33,61 @@ class TagViewController: UIViewController {
     }
     
     // MARK: - Function
+    private func setCollectionView() {
+        registerXib()
+        dataSource = UICollectionViewDiffableDataSource<Section, Album>(collectionView: albumCollectionView, cellProvider: { collectionView, indexPath, item in
+            guard let albumCell = collectionView.dequeueReusableCell(withReuseIdentifier: Const.Identifier.TagAlbumCollectionViewCell, for: indexPath) as?  TagAlbumCollectionViewCell else { fatalError() }
+            albumCell.setDumy(item)
+            return albumCell
+        })
+        applyInitialDataSource()
+    }
     
+    private func registerXib() {
+        albumCollectionView.register(UINib(nibName: Const.Identifier.TagAlbumCollectionViewCell, bundle: nil),
+                                     forCellWithReuseIdentifier: Const.Identifier.TagAlbumCollectionViewCell)
+    }
     
-    // MARK: - Objc Function
+    private func applyInitialDataSource() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Album>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(albumList, toSection: .main)
+        dataSource.apply(snapshot)
+        albumCollectionView.collectionViewLayout = layout()
+    }
     
+    private func layout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize( widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(110.0))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top:  15, leading: 20, bottom: 15, trailing: 20)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+    
+}
 
-    // MARK: - IBAction
-
-    
+extension Album {
+    static let list = [
+        Album(isMarked: true, name: "tag"),
+        Album(isMarked: true, name: "instagram"),
+        Album(isMarked: true, name: "youtube"),
+        Album(isMarked: false, name: "cafe"),
+        Album(isMarked: false, name: "dog"),
+        Album(isMarked: false, name: "cat"),
+        Album(isMarked: false, name: "tag"),
+        Album(isMarked: false, name: "tag"),
+        Album(isMarked: false, name: "tag"),
+        Album(isMarked: false, name: "tag"),
+        Album(isMarked: false, name: "tag"),
+        Album(isMarked: false, name: "tag"),
+        Album(isMarked: false, name: "tag"),
+        Album(isMarked: false, name: "tag"),
+        Album(isMarked: false, name: "tag"),
+        Album(isMarked: false, name: "tag")
+    ]
 }
