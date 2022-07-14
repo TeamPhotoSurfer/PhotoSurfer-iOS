e617b568082118dded8a206611fd04bad5b093da
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-07-15 03:02:43 +0900

    [#31] 헤더추가

--- PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/HomeSearch/Controllers/HomeSearchViewController+Extension.swift
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/HomeSearch/Controllers/HomeSearchViewController+Extension.swift
@@ -18,8 +18,24 @@ extension HomeSearchViewController {
             widthDimension: .fractionalWidth(1.0),
             heightDimension: .absolute(34))
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        group.interItemSpacing = .fixed(12)
         let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 12
+        section.contentInsets = NSDirectionalEdgeInsets(top: 4, leading: 20, bottom: 8, trailing: 20)
+        let headerSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(48))
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+                   layoutSize: headerSize,
+                   elementKind: UICollectionView.elementKindSectionHeader,
+                   alignment: .top)
+        section.boundarySupplementaryItems = [header]
         let layout = UICollectionViewCompositionalLayout(section: section)
         return layout
     }
+}
+
+extension HomeSearchViewController: UISearchBarDelegate {
+    
+    
 }
--- PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/HomeSearch/Controllers/HomeSearchViewController.swift
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/HomeSearch/Controllers/HomeSearchViewController.swift
@@ -28,6 +28,7 @@ final class HomeSearchViewController: UIViewController {
     var recentTags: [Tag] = []
     var frequencyTags: [Tag] = []
     var relatedTags: [Tag] = []
+    var collectionViewHeaders = ["입력한 태그", "최근 추가한 태그", "자주 추가한 태그", "연관 태그"]
     
     // MARK: - IBOutlet
     @IBOutlet weak var searchBar: UISearchBar!
@@ -40,6 +41,7 @@ final class HomeSearchViewController: UIViewController {
         setDummy()
         setUI()
         setCollectionView()
+        setSearchBarDelegate()
     }
     
     // MARK: - Function
@@ -54,20 +56,35 @@ final class HomeSearchViewController: UIViewController {
         searchBar.searchTextField.font = .iosBody2
     }
     
+    private func setSearchBarDelegate() {
+        searchBar.delegate = self
+    }
+    
     private func setCollectionView() {
         registerXib()
         collectionView.setCollectionViewLayout(createLayout(), animated: true)
-        dataSource = UICollectionViewDiffableDataSource<Section, Tag>(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
-            guard let tagCell = collectionView.dequeueReusableCell(withReuseIdentifier: Const.Identifier.TagCollectionViewCell, for: indexPath) as? TagCollectionViewCell else { fatalError() }
-            tagCell.setData(title: itemIdentifier.title)
-            return tagCell
-        })
+        setDataSource()
         applyInitialDataSource()
     }
     
     private func registerXib() {
         collectionView.register(UINib(nibName: Const.Identifier.TagCollectionViewCell, bundle: nil),
                                 forCellWithReuseIdentifier: Const.Identifier.TagCollectionViewCell)
+        collectionView.register(UINib(nibName: Const.Identifier.HomeSearchHeaderCollectionReusableView, bundle: nil),
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: Const.Identifier.HomeSearchHeaderCollectionReusableView)
+    }
+    
+    private func setDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Tag>(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            guard let tagCell = collectionView.dequeueReusableCell(withReuseIdentifier: Const.Identifier.TagCollectionViewCell, for: indexPath) as? TagCollectionViewCell else { fatalError() }
+            tagCell.setData(title: itemIdentifier.title)
+            return tagCell
+        })
+        dataSource.supplementaryViewProvider = {(collectionView, kind, indexPath) -> UICollectionReusableView in
+            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: Const.Identifier.HomeSearchHeaderCollectionReusableView, for: indexPath) as? HomeSearchHeaderCollectionReusableView else { fatalError() }
+            header.setData(title: self.collectionViewHeaders[indexPath.section])
+            return header
+        }
     }
     
     private func applyInitialDataSource() {
