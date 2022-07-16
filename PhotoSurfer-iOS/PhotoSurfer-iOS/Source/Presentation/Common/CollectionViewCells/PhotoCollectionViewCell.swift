7d27cce0e09802a551c48b43ff93133b9f5a1d8b
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-07-16 16:51:21 +0900

    [#35] 검색결과 UI 파일 추가

---
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Common/CollectionViewCells/PhotoCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoSurfer-iOS
+//
+//  Created by 김혜수 on 2022/07/16.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
