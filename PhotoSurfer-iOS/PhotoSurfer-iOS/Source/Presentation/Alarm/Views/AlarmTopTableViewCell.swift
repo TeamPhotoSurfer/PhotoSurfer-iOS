21c430337c34d4fcef10bd5a7249086767c37591
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-07-18 04:41:49 +0900

    [#47] 에셋 추가

--- PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Alarm/Views/AlarmTopTableViewCell.swift
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Alarm/Views/AlarmTopTableViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AlarmTopTableViewCell: UITableViewCell {
+final class AlarmTopTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
