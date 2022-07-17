dcfbe5b4b8b980eba909e342f79a3ddddf959f94
khnpoq@naver.com <khnpoq@naver.com>
2022-07-18 03:45:25 +0900

    [#42] 키보드 제어 추가

--- PhotoSurfer-iOS/ImageShareExtension/Presentation/Alarm/Controllers/SetAlarmViewController.swift
+++ PhotoSurfer-iOS/ImageShareExtension/Presentation/Alarm/Controllers/SetAlarmViewController.swift
@@ -15,12 +15,14 @@ class SetAlarmViewController: UIViewController {
     
     // MARK: - Property
     let textViewPlaceHolder: String = "50자 이내로 알림메모를 작성해보세요."
+    var keyHeight: CGFloat?
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setUI()
+        setKeyboard()
     }
     
     // MARK: - Function
@@ -30,9 +32,30 @@ class SetAlarmViewController: UIViewController {
         setTextViewTextStyle()
     }
     
+    private func setKeyboard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        hideKeyboardWhenTappedAround()
+    }
+    
     private func setTextViewTextStyle() {
         memoTextView.textContainerInset = UIEdgeInsets(top: 12, left: 12, bottom: 12, right: 12)
         memoTextView.layer.cornerRadius = 8
+    }
+    
+    // MARK: - Objc Function
+    @objc func keyboardWillShow(_ sender: Notification) {
+        let userInfo: NSDictionary = sender.userInfo! as NSDictionary
+        let keyboardFrame: NSValue = userInfo.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue
+        let keyboardRectangle = keyboardFrame.cgRectValue
+        let keyboardHeight = keyboardRectangle.height
+        keyHeight = keyboardHeight
+
+        self.view.frame.size.height -= keyboardHeight
+    }
+    
+    @objc func keyboardWillHide(_ sender: Notification) {
+        self.view.frame.size.height += keyHeight!
     }
     
     // MARK: - IBAction
