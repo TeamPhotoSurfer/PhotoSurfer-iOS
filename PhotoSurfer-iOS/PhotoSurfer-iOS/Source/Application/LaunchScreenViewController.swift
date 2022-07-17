550baa33633ef8a05d1856e1d4cc0ab55a3400b6
jeongkite <jeong6kite@gmail.com>
2022-07-18 03:06:26 +0900

    [X] 파일 추가

---
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Application/LaunchScreenViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LaunchScreenViewController.swift
+//  PhotoSurfer-iOS
+//
+//  Created by 정연 on 2022/07/18.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
