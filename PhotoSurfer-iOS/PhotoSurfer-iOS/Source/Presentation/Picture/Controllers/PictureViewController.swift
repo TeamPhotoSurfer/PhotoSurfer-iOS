afcc5bb05fc7d72d960d3473cbdfd3697b8863f8
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-07-17 17:53:46 +0900

    [#37] 사진 크기 인식 후 가운데 정렬 Or 스크롤

--- PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Picture/Controllers/PictureViewController.swift
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Picture/Controllers/PictureViewController.swift
@@ -8,22 +8,26 @@
 import UIKit
 
 final class PictureViewController: UIViewController {
-
+    
+    // MARK: - Property
+    let photo = Const.Image.imgSea
+    
+    // MARK: - IBOutlet
+    @IBOutlet weak var imageBackgroundView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageContainerView: UIView!
+    @IBOutlet weak var scrollViewHeight: NSLayoutConstraint!
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setImageData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Function
+    private func setImageData() {
+        imageView.image = photo
+        scrollViewHeight.priority = UILayoutPriority(photo.size.height <= photo.size.width ? 1000 : 250)
     }
-    */
-
 }
