3cb13663ecb4e7050a8393b032229e72e6c0b171
khnpoq@naver.com <khnpoq@naver.com>
2022-07-18 15:16:06 +0900

    [#42] textView 제어 구현

--- PhotoSurfer-iOS/ImageShareExtension/Presentation/Alarm/Controllers/SetAlarmViewController.swift
+++ PhotoSurfer-iOS/ImageShareExtension/Presentation/Alarm/Controllers/SetAlarmViewController.swift
@@ -100,3 +100,28 @@ class SetAlarmViewController: UIViewController {
         self.navigationController?.pushViewController(setRepresentTagViewController, animated: true)
     }
 }
+
+extension SetAlarmViewController: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == textViewPlaceHolder {
+            textView.text = ""
+            textView.textColor  = .grayGray90
+        }
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        if textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            textView.text = textViewPlaceHolder
+            textView.textColor = .grayGray50
+        }
+    }
+}
