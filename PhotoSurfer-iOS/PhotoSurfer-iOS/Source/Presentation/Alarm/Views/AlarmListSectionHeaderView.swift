139bb06a96042801e2d9000a64ec9411f1aa67bc
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-07-18 17:34:21 +0900

    [#47] 헤더뷰 이동

--- PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Alarm/Views/AlarmListSectionHeaderView.swift
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Alarm/Views/AlarmListSectionHeaderView.swift
@@ -7,14 +7,44 @@
 
 import UIKit
 
-class AlarmListSectionHeaderView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class AlarmListSectionHeaderView: UIView {
+    
+    // MARK: - Component
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .iosCaption1
+        label.textColor = .grayGray90
+        return label
+    }()
+    
+    // MARK: - LifeCycle
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        setLayout()
     }
-    */
-
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLayout()
+    }
+    
+    init(title: String) {
+        super.init(frame: CGRect.zero)
+        setLayout()
+        setTitle(title)
+    }
+    
+    // MARK: - Function
+    private func setLayout() {
+        self.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -12)
+        ])
+    }
+    
+    private func setTitle(_ title: String) {
+        titleLabel.text = title
+    }
 }
