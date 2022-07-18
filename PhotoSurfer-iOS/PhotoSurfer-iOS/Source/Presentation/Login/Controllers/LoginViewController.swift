d0d59e5f93e5f8cfbb27b73ce9069a1ce071e85a
jeongkite <jeong6kite@gmail.com>
2022-07-18 20:06:41 +0900

    [#48] 디바이더 구성

--- PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Login/Controllers/LoginViewController.swift
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Login/Controllers/LoginViewController.swift
@@ -8,22 +8,28 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    // MARK: - Property
+    var gradientLayer: CAGradientLayer!
 
+    // MARK: - IBOutlet
+    @IBOutlet weak var backgroundView: UIView!
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Function
+    func setUI() {
+        self.gradientLayer = CAGradientLayer()
+        self.gradientLayer.frame = self.view.bounds
+        self.gradientLayer.colors = [UIColor.splashGradientTop.cgColor, UIColor.splashGradientBottom.cgColor]
+        self.gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        self.gradientLayer.endPoint = CGPoint(x: 0, y: 0.5)
+        backgroundView.layer.addSublayer(self.gradientLayer)
     }
-    */
 
 }
