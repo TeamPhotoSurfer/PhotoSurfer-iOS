729b7ec42e082efa5b7c773e1bff067029d499dd
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-07-19 02:20:14 +0900

    [#53] 서버통신 URLConstant 세팅
    closed #53

--- PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Service/Network/URLConstant.swift
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Service/Network/URLConstant.swift
@@ -9,5 +9,26 @@ import Foundation
 
 struct URLConstant {
     
+    // MARK: - Base
     static let baseURL = ""
+    
+    // MARK: - Auth
+    static let authLogin = "/auth/login"
+    
+    // MARK: - Tag
+    static let tagMain = "/tag/main"
+    static let tagSearch = "/tag/search"
+    static let tag = "/tag"
+    static let tagBookmark = "/tag/bookmark"
+    
+    // MARK: - Photo
+    static let photo = "/photo"
+    static let photoTag = "/photo/tag"
+    static let photoPush = "/photo/push"
+    
+    // MARK: - Push
+    static let push = "/push"
+    static let pushLast = "/push/last"
+    static let pushCome = "/push/come"
+    static let pushToday = "/push/today"
 }
