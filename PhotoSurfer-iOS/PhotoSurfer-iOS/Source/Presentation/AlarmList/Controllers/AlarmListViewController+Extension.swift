19b4e34620b019a201d1ac7c3b0a5b5f39497602
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-07-18 18:49:36 +0900

    [X] 파일추가

---
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/AlarmList/Controllers/AlarmListViewController+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  AlarmListViewController+Extension.swift
+//  PhotoSurfer-iOS
+//
+//  Created by 김혜수 on 2022/07/18.
+//
+
+import Foundation
---
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/AlarmList/Controllers/AlarmListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AlarmListViewController.swift
+//  PhotoSurfer-iOS
+//
+//  Created by 김혜수 on 2022/07/18.
+//
+
+import UIKit
+
+class AlarmListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
