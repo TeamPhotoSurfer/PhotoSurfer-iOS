61b7fc9629e548a594a784e8c0889acb3c45fb39
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-07-18 16:34:27 +0900

    [#47] 테이블뷰 세팅

--- PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Alarm/Controllers/AlarmViewController+Extension.swift
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Alarm/Controllers/AlarmViewController+Extension.swift
@@ -5,4 +5,19 @@
 //  Created by 김혜수 on 2022/07/18.
 //
 
-import Foundation
+import UIKit
+
+extension AlarmViewController: UITableViewDelegate {
+    
+}
+
+extension AlarmViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Const.Identifier.AlarmListTableViewCell, for: indexPath) as? AlarmListTableViewCell else { fatalError() }
+        return cell
+    }
+}
--- PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Alarm/Controllers/AlarmViewController.swift
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Alarm/Controllers/AlarmViewController.swift
@@ -9,8 +9,27 @@ import UIKit
 
 final class AlarmViewController: UIViewController {
     
+    // MARK: - IBOutlet
+    @IBOutlet weak var tableHeaderView: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    // MARK: - Function
+    private func setTableView() {
+        registerXib()
+        tableView.contentInsetAdjustmentBehavior = .never
+        tableView.tableHeaderView = tableHeaderView
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.automaticallyAdjustsScrollIndicatorInsets = false
+    }
+    
+    private func registerXib() {
+        tableView.register(UINib(nibName: Const.Identifier.AlarmListTableViewCell, bundle: nil),
+                           forCellReuseIdentifier: Const.Identifier.AlarmListTableViewCell)
     }
 }
