0bf700eb629611dc274a0a54c459a901b001fc93
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-07-19 01:49:03 +0900

    [#63] 컴포넌트 배치

--- PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/AlarmDetail/Controllers/AlarmDetailViewController.swift
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/AlarmDetail/Controllers/AlarmDetailViewController.swift
@@ -9,7 +9,31 @@ import UIKit
 
 final class AlarmDetailViewController: UIViewController {
 
+    // MARK: - IBOutlet
+    @IBOutlet weak var dateButton: UIButton!
+    @IBOutlet weak var memoTextView: UITextView!
+    @IBOutlet weak var completeButton: UIButton!
+    
+    // MARK: - LifeCyele
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUI()
+    }
+    
+    // MARK: - Function
+    private func setUI() {
+        dateButton.layer.cornerRadius = 8
+        completeButton.layer.cornerRadius = 8
+        memoTextView.layer.cornerRadius = 8
+        memoTextView.textContainerInset = UIEdgeInsets(top: 12, left: 12, bottom: 12, right: 12)
+    }
+    
+    // MARK: - IBAction
+    @IBAction func dismissButtonDidTap(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func completeButtonDidTap(_ sender: Any) {
     }
 }
