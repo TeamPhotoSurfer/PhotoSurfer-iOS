fcd3d69d26f80ff7d80e8abf6b7bac3099de9f31
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-07-18 17:32:48 +0900

    [#47] 파일추가

---
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Alarm/Views/AlarmListSectionHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  AlarmListSectionHeaderView.swift
+//  PhotoSurfer-iOS
+//
+//  Created by 김혜수 on 2022/07/18.
+//
+
+import UIKit
+
+class AlarmListSectionHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
