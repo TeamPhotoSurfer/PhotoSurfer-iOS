666ab00c61dd1e48449cd37f27eda46855412729
jeongkite <jeong6kite@gmail.com>
2022-07-19 16:12:46 +0900

    [#24] UIButton Extension 수정

--- PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Common/Extensions/UIButton+.swift
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Common/Extensions/UIButton+.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 extension UIButton {
-    func setTagName(button: UIButton, name: String) {
+    func setTagName(name: String) {
         let attributedString = NSMutableAttributedString(string: "")
         let imageAttachment = NSTextAttachment()
         imageAttachment.image = Const.Image.icHashtagLineTagWhite
         imageAttachment.bounds = CGRect(x: 0, y: -3, width: 16, height: 16)
         attributedString.append(NSAttributedString(attachment: imageAttachment))
         attributedString.append(NSAttributedString(string: name))
-        button.setAttributedTitle(attributedString, for: .normal)
-        button.titleLabel?.textAlignment = NSTextAlignment.center
+        self.setAttributedTitle(attributedString, for: .normal)
+        self.titleLabel?.textAlignment = NSTextAlignment.center
     }
 }
