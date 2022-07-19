b7cbcc05f8723245217876db53690ee22fd5b558
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-07-19 21:55:56 +0900

    [#72] 태그수정, 태그 추가 시 키보드 제어

--- PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Picture/Controllers/PictureViewController.swift
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Presentation/Picture/Controllers/PictureViewController.swift
@@ -7,11 +7,20 @@
 
 import UIKit
 
+import IQKeyboardManagerSwift
+
 final class PictureViewController: UIViewController {
     
     enum ViewType {
         case picture
         case alarmSelected
+    }
+    
+    enum PictureEditMode {
+        case none
+        case add
+        case delete
+        case edit
     }
     
     enum Section {
@@ -21,6 +30,7 @@ final class PictureViewController: UIViewController {
     // MARK: - Property
     let photo = Const.Image.imgSea
     var type: ViewType = .alarmSelected
+    var editMode: PictureEditMode = .none
     var dataSource: UICollectionViewDiffableDataSource<Section, Tag>!
     var tags = [Tag(title: "tag1ㅋㅋㅋㅋㅋㅋㅋㅋㅋ"), Tag(title: "tag1"), Tag(title: "tag1"), Tag(title: "tag1")]
     
@@ -35,19 +45,35 @@ final class PictureViewController: UIViewController {
     @IBOutlet weak var alarmDetailButton: UIButton!
     @IBOutlet weak var bottomWaveView: UIView!
     @IBOutlet weak var moreButton: UIButton!
+    @IBOutlet weak var waveViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var keyboardTopTextField: UITextField!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var bottomShareButton: UIButton!
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setViewType(type: type)
-        setUI()
+        setUI(editMode: editMode)
         setImageData()
         setCollectionView()
         setMoreButtonMenu()
+        addKeyboardObserver()
+        setKeyboardManagerEnable(false)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        removeKeyboardObserver()
+        setKeyboardManagerEnable(true)
     }
     
     // MARK: - Function
+    private func setKeyboardManagerEnable(_ isEnabled: Bool) {
+        IQKeyboardManager.shared.enable = isEnabled
+    }
+    
     private func setViewType(type: ViewType) {
         [alarmDetailButton, shareButton].forEach {
             $0?.isHidden = (type == .picture)
@@ -57,8 +83,28 @@ final class PictureViewController: UIViewController {
         }
     }
     
-    private func setUI() {
+    private func setUI(editMode: PictureEditMode) {
         alarmDetailButton.layer.cornerRadius = 8
+        keyboardTopTextField.layer.cornerRadius = 22
+        keyboardTopTextField.layer.borderColor = UIColor.grayGray30.cgColor
+        keyboardTopTextField.layer.borderWidth = 1
+        keyboardTopTextField.addPadding(padding: 12)
+        switch editMode {
+        case .add:
+            deleteButton.isHidden = true
+            bottomShareButton.isHidden = true
+            keyboardTopTextField.isHidden = false
+        case .delete:
+            print("delete")
+        case .edit:
+            deleteButton.isHidden = true
+            bottomShareButton.isHidden = true
+            keyboardTopTextField.isHidden = false
+        case .none:
+            deleteButton.isHidden = false
+            bottomShareButton.isHidden = false
+            keyboardTopTextField.isHidden = true
+        }
     }
     
     private func setImageData() {
@@ -114,16 +160,44 @@ final class PictureViewController: UIViewController {
     
     private func setMoreButtonMenu() {
         let addTag = UIAction(title: "태그추가") { action in
-            
+            self.editMode = .add
+            self.setUI(editMode: self.editMode)
+            self.keyboardTopTextField.becomeFirstResponder()
         }
         let deleteTag = UIAction(title: "태그삭제") { action in
-           
+            
         }
         let editTag = UIAction(title: "태그수정") { action in
-            
+            self.editMode = .edit
+            self.setUI(editMode: self.editMode)
+            self.keyboardTopTextField.becomeFirstResponder()
         }
         moreButton.menu = UIMenu(title: "", children: [addTag, deleteTag, editTag])
         moreButton.showsMenuAsPrimaryAction = true
+    }
+    
+    private func addKeyboardObserver() {
+        NotificationCenter.default.addObserver(self, selector:#selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector:#selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    private func removeKeyboardObserver() {
+        NotificationCenter.default.removeObserver(UIResponder.keyboardWillShowNotification)
+        NotificationCenter.default.removeObserver(UIResponder.keyboardWillHideNotification)
+    }
+    
+    // MARK: - Objc Function
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardHeight = keyboardFrame.cgRectValue.height
+            waveViewBottomConstraint.constant = keyboardHeight
+        }
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        editMode = .none
+        setUI(editMode: editMode)
+        waveViewBottomConstraint.constant = 0
     }
     
     // MARK: - IBAction
