b75177993c166aad997f9b8fa487d7e1626203e5
khnpoq@naver.com <khnpoq@naver.com>
2022-07-20 19:40:37 +0900

    [X] tag 태그명 조회 Response추가

--- PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Service/Data/TagModel/TagResponse.swift
+++ PhotoSurfer-iOS/PhotoSurfer-iOS/Source/Service/Data/TagModel/TagResponse.swift
@@ -23,3 +23,30 @@ struct Tag: Codable, Hashable {
     var id: Int? = 0
     let name: String
 }
+
+/// 태그명 조회
+struct TagBookMarkedReponse: Codable {
+    var bookmarked: [TagInfo]?
+    var notBookmarked: [TagInfo]
+}
+
+struct TagInfo: Codable {
+    let id: Int
+    let name: String
+    let bookmarkStatus: Bool
+    let imageURL: String
+    let tagType: TagType
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case bookmarkStatus = "bookmark_status"
+        case imageURL = "image_url"
+        case tagType = "tag_type"
+    }
+}
+
+enum TagType: String, Codable {
+    case general = "general"
+    case platform = "platform"
+    case tagTypeGENERAL = "GENERAL"
+}
