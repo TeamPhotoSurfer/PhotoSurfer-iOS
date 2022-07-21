e7c13eb2859527ec4b165d323d51acfc7aef390a
khnpoq@naver.com <khnpoq@naver.com>
2022-07-22 02:04:14 +0900

    [#150] 검색 뷰 오류 수정

--- PhotoSurfer-iOS/ImageShareExtension/Source/Presentation/Tag/Controllers/ShareViewController+Extension.swift
+++ PhotoSurfer-iOS/ImageShareExtension/Source/Presentation/Tag/Controllers/ShareViewController+Extension.swift
@@ -267,7 +267,7 @@ extension ShareViewController: UICollectionViewDelegate {
         case 0:
             setDeselectedTagUI(indexPath: indexPath, collectionView: collectionView)
             addedTags.remove(at: indexPath.item)
-            if addedTags.count <= 0 {
+            if addedTags.count < 0 {
                 UIView.animate(withDuration: 0.5) {
                     self.typingViewTopConstraint.constant += 34
                 }
@@ -294,7 +294,7 @@ extension ShareViewController: UICollectionViewDelegate {
             else {
                 addedTags.append(Tag(name: tagType[indexPath.item].name, bookmarkStatus: nil, imageURL: nil, tagType: nil))
                 setTagUI(indexPath: indexPath, collectionView: collectionView, isAdded: true)
-                if addedTags.count <= 0 {
+                if addedTags.count < 0 {
                     UIView.animate(withDuration: 0.5) {
                         self.typingViewTopConstraint.constant = self.typingButtonTopConstValue
                     }
